21a75a58c2742ceecb5fbc04af39c1145e364df4
Bill Mageni <+>
2024-06-13 13:47:28 +0300

    Fixed seedieng file

--- lib/seed.py
+++ lib/seed.py
@@ -29,21 +29,17 @@ def seed_database():
     print("Seeded database")
 
 def seed_products():
-    # Seed products
-    product1 = Product.create("Laptop", 1000.00, "High-performance laptop")
-    product2 = Product.create("T-Shirt", 20.00, "Comfortable and stylish T-shirt")
+    product1 = Product.create("Laptop", 1000.00, "High-performance laptop",1)
+    product2 = Product.create("T-Shirt", 20.00, "Comfortable and stylish T-shirt",2)
 
 def seed_suppliers():
-    # Seed suppliers
     supplier1 = Supplier.create("TechGiant Inc.", "123 Main St")
     supplier2 = Supplier.create("Fashion Emporium", "456 Elm St")
 
 def seed_order_history():
-    # Retrieve Product objects
     product1 = Product.find_by_id(1)
     product2 = Product.find_by_id(2)
 
-    # Seed order history
     if product1 and product2:
         order_history1 = OrderHistory.create(product1, 2, "2024-06-10")
         order_history2 = OrderHistory.create(product2, 5, "2024-06-11")
@@ -52,13 +48,11 @@ def seed_order_history():
 
 
 def seed_order_details():
-    # Retrieve Product and Supplier objects
     product1 = Product.find_by_id(1)
     supplier1 = Supplier.find_by_id(1)
     product2 = Product.find_by_id(2)
     supplier2 = Supplier.find_by_id(2)
 
-    # Seed order details
     if product1 and supplier1 and product2 and supplier2:
         order_detail1 = OrderDetails.create(product1, supplier1, 2, "2024-06-10")
         order_detail2 = OrderDetails.create(product2, supplier2, 5, "2024-06-11")
@@ -66,7 +60,6 @@ def seed_order_details():
         print("Error: Product or Supplier not found")
 
 def seed_categories():
-    # Seed categories
     category1 = Category.create("Electronics")
     category2 = Category.create("Clothing")
 
